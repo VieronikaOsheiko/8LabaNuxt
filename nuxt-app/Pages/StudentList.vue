f097ec0872c7dfe92e4d592c688544f983c5dfa2
VieronikaOsheiko <vieronika.osheiko@oa.edu.ua>
2024-03-31 14:51:02 +0300

    Feat: Create table list of student with search and paginable

---
+++ nuxt-app/Pages/StudentList.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+const columns = [{
+  key: 'id',
+  label: 'ID'
+}, {
+  key: 'name',
+  label: 'Name'
+}, {
+  key: 'title',
+  label: 'Title'
+}, {
+  key: 'email',
+  label: 'Email'
+}, {
+  key: 'role',
+  label: 'Role'
+}]
+
+const people = ref([{
+  id: 1,
+  name: 'Lindsay Walton',
+  title: 'Front-end Developer',
+  email: 'lindsay.walton@example.com',
+  role: 'Member'
+}, {
+  id: 2,
+  name: 'Courtney Henry',
+  title: 'Designer',
+  email: 'courtney.henry@example.com',
+  role: 'Admin'
+}, {
+  id: 3,
+  name: 'Tom Cook',
+  title: 'Director of Product',
+  email: 'tom.cook@example.com',
+  role: 'Member'
+}, {
+  id: 4,
+  name: 'Whitney Francis',
+  title: 'Copywriter',
+  email: 'whitney.francis@example.com',
+  role: 'Admin'
+}, {
+  id: 5,
+  name: 'Leonard Krasner',
+  title: 'Senior Designer',
+  email: 'leonard.krasner@example.com',
+  role: 'Owner'
+}, {
+  id: 6,
+  name: 'Floyd Miles',
+  title: 'Principal Designer',
+  email: 'floyd.miles@example.com',
+  role: 'Member'
+}])
+
+const page = ref(1)
+const pageCount = 5
+
+const q = ref('')
+
+const filteredRows = computed(() => {
+  if (!q.value) {
+    return people.value
+  }
+
+  return people.value.filter((person) => {
+    return Object.values(person).some((value) => {
+      return String(value).toLowerCase().includes(q.value.toLowerCase())
+    })
+  })
+})
+
+const paginatedRows = computed(() => {
+  return filteredRows.value.slice((page.value - 1) * pageCount, page.value * pageCount)
+})
+</script>
+
+<template>
+  <div>
+    <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
+      <UInput v-model="q" placeholder="Filter people..." />
+    </div>
+
+    <UTable :rows="paginatedRows" :columns="columns" />
+
+    <div class="flex justify-end px-3 py-3.5 border-t border-gray-200 dark:border-gray-700">
+      <UPagination v-model="page" :page-count="pageCount" :total="filteredRows.length" />
+    </div>
+  </div>
+</template>
--- nuxt-app/Pages/index.vue
+++ nuxt-app/Pages/index.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 
+import StudentList from "~/Pages/StudentList.vue";
 </script>
-
 <template>
-
+<student-list></student-list>
 </template>
 
 <style scoped>
